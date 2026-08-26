6c8e5952cfc5dc6c85f9fec7950b4820bb6a0e28
Lucca <luccabenedetti@protonmail.com>
2026-08-26 13:33:55 -0300

    refactor(ponytail): remove confirmed dead code

--- services/program/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './crud';
-export * from './weeks';
-export * from './targets';
-export * from './dashboard';
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export { parseTargetSets, getRirColor } from './exercise';
-export { formatTimer } from './timer';
-export { calculateWarmupProgression, formatWarmupProgression, getWarmupReps } from './warmup';
-export { calculateVolume } from './calculations';
-export { getISOWeek, getWeekStart, formatEpochDate, formatDateShort, getWeekNumber } from './date-utils';
